415f913162849dbf8b4d92621a120be792f01fc8
Ilya Belyavtsev <ilya.belyavtsev@solarwinds.com>
2018-11-17 07:58:22 +0300

    Copying to ostream_iterator and constant iterators added, project is now separated into header file and source file

--- main.cpp
+++ main.cpp
@@ -1,124 +1,14 @@
-#include <iostream>
-#include <memory>
-#include <algorithm>
-#include <cassert>
+#include "rule_of_five.h"
+
+using namespace MyArray;
 
 using std::cout;
 using std::endl;
 
-template <class T>
-class MyArray{
-private:
-    T *ptr;
-    size_t size;
-
-    using index_type = size_t;
-
-    //Destroy and deallocate
-    void destructMyself(){
-        std::destroy(begin(), end());
-        free(ptr);
-    }
-
-    //For the move constructor and the assignment operator
-    void stealObject(MyArray&& rhs){
-        size = rhs.size;
-        ptr = rhs.ptr;
-        rhs.ptr = nullptr;
-    }
-
-public:
-    MyArray(size_t size_0, T init_value = T{}) : size(size_0) {
-
-        cout << "Constructor was called with size = " << size
-             << " and initial value = " << init_value << endl;
-
-        ptr = static_cast<T*>(malloc(size*sizeof(T)));
-
-        std::fill(begin(), end(), init_value);
-    }
-
-    MyArray(const MyArray& rhs) : size(rhs.size) {
-
-        cout << "Copy constructor was called" << endl;
-
-        ptr = static_cast<T*>(malloc(size*sizeof(T)));
-
-        std::copy(rhs.begin(), rhs.end(), begin());
-    }
-
-    MyArray& operator = (const MyArray& rhs){
-
-        cout << "Copy assignment operator was called" << endl;
-
-        assert(this != &rhs);
-
-        size = rhs.size;
-
-        ptr = static_cast<T*>(realloc(ptr, size));
-
-        std::copy(rhs.begin(), rhs.end(), begin());
-
-        return *this;
-    }
-
-    MyArray(MyArray&& rhs) {
-
-        cout << "Move constructor was called" << endl;
-
-        stealObject(std::move(rhs));
-    }
-
-    MyArray& operator = (MyArray&& rhs) {
-
-        cout << "Move assignment operator was called" << endl;
-
-        assert(this != &rhs);
-
-        destructMyself();
-
-        stealObject(std::move(rhs));
-
-        return *this;
-    }
-
-    T& operator[] (index_type index){
-        assert(index < size);
-        return *(ptr+index);
-    }
-
-    const T& operator[] (index_type index) const{
-        assert(index < size);
-        return *(ptr+index);
-    }
-
-    T* begin() const{
-        return ptr;
-    }
-
-    T* end() const{
-        return ptr+size;
-    }
-
-    ~MyArray(){
-        if (ptr != nullptr){
-            destructMyself();
-        }
-    }
-};
-
-template <class T>
-std::ostream& operator << (std::ostream& stream, const MyArray<T>& arr){
-
-    std::for_each(arr.begin(), arr.end(), [&stream](const auto& elt){stream << elt << endl;});
-
-    return stream;
-}
+using IntegerArray = MyArray<int>;
 
 int main()
 {
-    using IntegerArray = MyArray<int>;
-
     //Usual constructor
     IntegerArray arr1(5);
 
@@ -128,11 +18,14 @@ int main()
     //Copy assignment operator
     arr1 = arr2;
 
-    //Move constructor?????
+    //Usual constructor! Our compiler is smart enough!
     auto arr3 { IntegerArray(10, 100) };
 
+    //Move constructor
+    auto arr4 { std::move(IntegerArray(10, 100)) };
+
     //Move assignment operator
-    arr3 = IntegerArray(10, 100);
+    arr4 = IntegerArray(10, 100);
 
     return 0;
 }
---
+++ rule_of_five.h
@@ -0,0 +1,177 @@
+#ifndef RULE_OF_FIVE_H
+#define RULE_OF_FIVE_H
+
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+
+namespace MyArray
+{
+
+using std::cout;
+using std::endl;
+
+template <class T>
+class MyArray {
+private:
+    T *ptr_;
+    size_t size_;
+
+    using index_type = size_t;
+
+    //Destroy and deallocate
+    void destructMyself();
+
+    //For the move constructor and the assignment operator
+    //Now takes lvalue ref, no need of std::move call in calling function
+    void stealObject(MyArray& rhs);
+
+public:
+    MyArray(size_t size_0, T init_value = T{});
+
+    MyArray(const MyArray& rhs);
+
+    MyArray& operator = (const MyArray& rhs);
+
+    MyArray(MyArray&& rhs);
+
+    MyArray& operator = (MyArray&& rhs);
+
+    T& operator[] (index_type index);
+
+    const T& operator[] (index_type index) const;
+
+    T* begin();
+
+    T* end();
+
+    const T* cbegin() const;
+
+    const T* cend() const;
+
+    ~MyArray();
+};
+
+template<class T>
+MyArray<T> :: MyArray(size_t size_0, T init_value) : size_(size_0) {
+
+    cout << "Constructor was called with size = " << size_
+         << " and initial value = " << init_value << endl;
+
+    ptr_ = static_cast<T*>(malloc(size_*sizeof(T)));
+
+    std::fill(begin(), end(), init_value);
+}
+
+template<class T>
+MyArray<T> :: MyArray(const MyArray& rhs) : size_(rhs.size_) {
+
+    cout << "Copy constructor was called" << endl;
+
+    ptr_ = static_cast<T*>(malloc(size_*sizeof(T)));
+
+    std::copy(rhs.cbegin(), rhs.cend(), begin());
+}
+
+template<class T>
+MyArray<T>& MyArray<T>::operator = (const MyArray& rhs){
+
+    cout << "Copy assignment operator was called" << endl;
+
+    assert(this != &rhs);
+
+    size_ = rhs.size_;
+
+    ptr_ = static_cast<T*>(realloc(ptr_, size_));
+
+    std::copy(rhs.cbegin(), rhs.cend(), begin());
+
+    return *this;
+}
+
+template<class T>
+MyArray<T> :: MyArray(MyArray&& rhs) {
+
+    cout << "Move constructor was called" << endl;
+
+    stealObject(rhs);
+}
+
+template<class T>
+MyArray<T>& MyArray<T>::operator = (MyArray&& rhs) {
+
+    cout << "Move assignment operator was called" << endl;
+
+    assert(this != &rhs);
+
+    destructMyself();
+
+    stealObject(rhs);
+
+    return *this;
+}
+
+template<class T>
+T& MyArray<T>::operator[] (index_type index){
+    assert(index < size_);
+    return *(ptr_+index);
+}
+
+template<class T>
+const T& MyArray<T>::operator[] (index_type index) const{
+    assert(index < size_);
+    return *(ptr_+index);
+}
+
+template<class T>
+T* MyArray<T>::begin() {
+    return ptr_;
+}
+
+template<class T>
+T* MyArray<T>::end() {
+    return ptr_ + size_;
+}
+
+template<class T>
+const T* MyArray<T>::cbegin() const {
+    return ptr_;
+}
+
+template<class T>
+const T* MyArray<T>::cend() const {
+    return ptr_ + size_;
+}
+
+template<class T>
+MyArray<T>::~MyArray(){
+    if (ptr_ != nullptr){
+        destructMyself();
+    }
+}
+
+template<class T>
+void MyArray<T>::destructMyself(){
+    std::destroy(begin(), end());
+    free(ptr_);
+}
+
+template<class T>
+void MyArray<T>::stealObject(MyArray& rhs){
+    size_ = rhs.size_;
+    ptr_ = rhs.ptr_;
+    rhs.ptr_ = nullptr;
+}
+
+//Overloaded << operator for our container
+template <class T>
+std::ostream& operator << (std::ostream& stream, const MyArray<T>& arr){
+    std::copy( arr.cbegin(), arr.cend(), std::ostream_iterator<T>(stream,"\n") );
+    return stream;
+}
+
+}
+
+#endif // RULE_OF_FIVE_H
